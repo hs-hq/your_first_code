3e1885267166d26d2c48c61f2fe2c4521f9575c0
Emrah010 <146966675+Emrah010@users.noreply.github.com>
2024-01-18 22:34:01 +0400

    Update Code

--- main.c
+++ main.c
@@ -1,9 +1,12 @@
 int main()
 {
   int i;
-  for (i = 0; i++ < 32;) // Change the loop limit to 32
+  for (i = 0; i < 32; i++) // Change the loop limit to 32
+  {
     printf("Hello School\n"); // Change the output message
+  }
   return 0;
 }
+
 
 
